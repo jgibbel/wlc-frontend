10f8b713da723d9af061f8d52e9372c4e60ca8a6
jgibbel <jamesgibbel@gmail.com>
2020-03-04 20:41:19 -0500

    01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+
+import Home from './Home';
+import Mission from './Mission';
+import Members from './Members';
+import Join from './Join';
+import Events from './Events';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="header">
+    <div className="logo"><Link className="" to="/"><h1 className="underline">WLC <span>for Social Impact</span></h1></Link></div>
+    <div className="nav">
+      <div className="navLink"><NavLink className="underline" to="/">WHO WE ARE</NavLink></div> 
+      <div className="navLink"><NavLink className="underline" to="/mission">MISSION</NavLink></div>  
+      <div className="navLink"><NavLink className="underline" to="/members">MEMBERS</NavLink></div>   
+      <div className="navLink"><NavLink className="underline" to="/join">JOIN</NavLink></div>   
+      <div className="navLink">login</div>   
+       
     </div>
+
+    </div>
+    <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/mission">
+          <Mission />
+        </Route>
+        <Route exact path="/members">
+          <Members />
+        </Route>
+        <Route path="/join">
+          <Join />
+        </Route>
+        <Route path="/events">
+          <Events />
+        </Route>
+    </Switch>  
+
+    
+
+    </Router>
   );
 }
 
---
+++ src/Members.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Members extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Members;
