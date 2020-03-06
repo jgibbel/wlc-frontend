1ce0e1091354b27b655d00955ccb1f993912e24a
jgibbel <jamesgibbel@gmail.com>
2020-03-05 19:54:46 -0500

    Updates

---
+++ precache-manifest.c65b5d897653a8c6b0e237de2307efa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12bdc1335b0c4b3ec4aa46fa0433a2b",
+    "url": "/wlc-frontend/index.html"
+  },
+  {
+    "revision": "b4386c7342eda3497eff",
+    "url": "/wlc-frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1f88791475ec1b81d78a",
+    "url": "/wlc-frontend/static/js/2.b4f9974a.chunk.js"
+  },
+  {
+    "revision": "627328b95b668335a3c0f9d5d64174b1",
+    "url": "/wlc-frontend/static/js/2.b4f9974a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4386c7342eda3497eff",
+    "url": "/wlc-frontend/static/js/main.dc553cee.chunk.js"
+  },
+  {
+    "revision": "8f8ff0cc036355690c68",
+    "url": "/wlc-frontend/static/js/runtime-main.4e41d65d.js"
+  }
+]);
