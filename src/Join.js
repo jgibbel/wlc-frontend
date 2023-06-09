dccc539266b5b8c06654610d33d2ad66b332236c
jgibbel <jamesgibbel@gmail.com>
2023-06-09 17:32:40 -0400

    application szn

--- src/Join.js
+++ src/Join.js
@@ -10,64 +10,63 @@ export class Join extends Component {
             <div className="page-container">
                 
                 <div className="tan-foot">
-                <p className="tan-text" style={{textAlign: 'center'}}>The Membership Application process typically opens in the spring.</p>
+                <p className="tan-text" style={{textAlign: 'center'}}>Applications due June 30, 2023. Link + details below.</p>
             </div>
                 <div className="indent">
-                <h1>Membership</h1>
-                <p className="indent-more p">Information on the ideal member profile, member expectations, member benefits and the process to apply for membership is included below.</p>
-                <h1>WLC Members Will Receive:</h1>
+                <p className="indent-more p"><b>The WLC is a network of ~60 female-identified, NYC-based social impact leaders who share resources and ideas; problem-solve together; and are invested in one another’s growth and success. Our members lead organizations across NYC with annual budgets ranging from $100,000 to $200,000,000.</b></p>
+                <p className="indent-more p"><b>Our membership process is rooted in values of equity and accessibility. Potential members must complete a simple application and meet the following criteria:</b></p>
+                <ol className="indent-more p">
+                    <li>Currently an Executive Director/Co-Executive Director/CEO/President of a social impact organization based in NYC; </li>
+                    <li>Have worked as an Executive Director/Co-Executive Director/CEO/President of a social impact organization for at least 12 months (can include work at prior organizations);</li>
+                    <li>Identify as female—transwomen, cis-gendered women, and nonbinary people who are interested in a female-identified space are welcome; and</li>
+                    <li>Be willing and able to commit to participating in the WLC community for an entire program year (typically mid-September – mid-June), including: </li>
+                </ol>
+                <div className='indent'>
                 <ul className="indent-more p">
-                    <li>Access to a safe, accessible and inclusive community of women leaders who can provide and request advice and support through: online meet-ups, in-person gatherings and an email community</li>
-                    <li>Access to a regularly updated crowdsourced resource list with information on: funding, best practices, training opportunities, leadership development opportunities, etc.</li>
-                    <li>Access to templates and policies used by other organizations</li>
-                    <li>Invitations to a series of four scheduled sessions held online and in-person throughout the year</li>
-                    <li>The opportunity to form or participate in smaller topic based meeting groups (Minerva Salons)</li>
-                    <li>An invitation to attend a restorative gathering to relax, reflect and connect with other WLC members typically offered in June (WLC Retreat)</li>
+                    <li>Attending (to the extent possible) 5 – 6 meetings/year. <ul>
+                        <li className='specialLi'>These meetings are the primary venue for members to provide/receive peer support; learn and share information and resources; build relationships; and hear guest speakers (generally subject matter experts in topics members express interest in).
+                        </li>
+                        <li>Meetings are generally 1.5 hours; scheduled at the beginning of the year; and scheduled on a rotation of days/times to account for as wide an array of schedules as possible. </li></ul>
+                        </li>
+                        <li>
+                        Actively participating in the WLC listserv (which is dynamic and resource-rich with information on funding, best practices, training opportunities, leadership development opportunities–in short, anything members find useful);   
+                        </li>
+                        <li>
+                        Sharing ideas, resources, thought-partnership with fellow members; and   
+                        </li>
+                        <li>
+                        Participating in an annual half-day retreat (usually held in person in Manhattan, with the date/time announced significantly in advance); and
+                        </li>
+                        <li>
+                        If interested, participating in one or more of the WLC’s topic-specific groups (e.g., Board leadership; Leadership Transitions). These smaller, topic-based groups are organized by members each program year, based on interest. 
+                        </li>
                 </ul>
                 </div>
-                <img className="lecture-photo" src={LecturePic} alt="Lecture Photo"/>
-                <p className="caption">2019 WLC Retreat featuring Guest Speaker-Toni Harris Quinnerly, Consultant & Senior Expert at Egon Zehnder's Leadership Advisory Practice</p>
+                </div>
+                {/* <img className="lecture-photo" src={LecturePic} alt="Lecture Photo"/>
+                <p className="caption">2019 WLC Retreat featuring Guest Speaker-Toni Harris Quinnerly, Consultant & Senior Expert at Egon Zehnder's Leadership Advisory Practice</p> */}
 
                 
                 <div className="indent">
-                <h1>WLC Members Will Be Expected To:</h1>
+                <p className="indent-more p"><b>The WLC is funded by membership dues—which are tiered based on organizational budget size. <i>Scholarships are available, and we’re committed to not turning anyone away due to money.</i> </b></p>
+                <p className="indent-more p"><b>2023 – 2024 Program Year Membership Dues:</b></p>
                 <ul className="indent-more p">
-                    <li>Uphold confidentiality and safety by ensuring what is said online and in in-person gatherings stays there </li>
-                    <li>Attend and fully participate in four sessions (typically 90 mins; 9AM or 5PM start time; held online or in a Manhattan location) which provide an opportunity for members to:</li>
-                    <li style={{listStyle: 'none'}}> 
-                        <ul>
-                            <li>provide and receive support from peers</li>
-                            <li>learn and share information and resources </li>
-                            <li>build relationships</li>
-                            <li>hear guest speakers who are subject matter experts in areas/topics members are interested in</li>
-                        </ul>
-                    </li>
-                    <li>Contribute meaningfully to the WLC email community by posting and responding to queries to share information and resources</li>
-                    <li>Pay an annual membership fee* to support WLC’s regular operations, including space rental for gatherings, administrative support for day-to-day operations, food, beverages, etc.  Membership fees are noted below:</li>
-                    <li style={{listStyle: 'none'}}> 
-                        <ul>
-                            <li>$550 for individuals leading social impact organizations with a budget of greater than $1M </li>
-                            <li>$275 for individuals leading social impact organizations with a budget of less than or equal to $1M</li>
-                        </ul>
-                    </li>
-                </ul>
-                <p className="indent-more p">(*Please note the membership fee does NOT cover participation in the WLC Retreat)</p>
-                <h1>The Ideal WLC Member Is/Will:</h1>
-                <ul className="indent-more p">
-                    <li>Currently an ED/CEO/President of a social impact organization</li>
-                    <li>Based in NYC and leads a social impact organization</li>
-                    <li>Eager to both contribute to and learn from a community of women peers working in the social impact sector </li>
-                    <li>Have worked as a CEO/ED/President at their current organization or including prior experience for at least 12 months</li>
-                    <li>Able to attend at least four WLC online or in-person gatherings annually</li>
-                    <li>Comfortable and empowered to share their organization’s best practices, learnings and organizational documents</li>
-                </ul>
+                    <li>$100 for leaders of organizations with a budget of $250K or less, or for leaders currently unaffiliated with an organization</li>
+                    <li>$300 for leaders of organizations with a budget of greater than $250K but less than $1.5M</li>
 
-                <p className="indent-more p">The WLC Membership typically runs from mid September through early to mid June.</p>
-                <p className="indent-more p">Lastly, the WLC has a limited budget to provide scholarships to cover a portion of the membership fee for women and trans people who are leading organizations with an annual budget less than or equal to $1M. If you are interested in applying for a scholarship please complete the section in the application noting your interest.</p>
-                <p>To be added to mailing list to receive information on the membership application process in the spring please send an email to assistant@wlcsocialimpact.com.</p>
+                    <li>$500 for leaders of organizations with a budget of greater than $1.5M but less than $5M</li>
+                    <li>Pay an annual membership fee* to support WLC’s regular operations, including space rental for gatherings, administrative support for day-to-day operations, food, beverages, etc.  Membership fees are noted below:</li>
+                    <li>
+                    $700 for leaders of organizations with a budget of greater than $5M
+                    </li>
+                </ul>
+                <p className="indent-more p"><b>If you’re interested in joining for our next program year (September 2023 – June 2024), please complete <a href="https://www.surveymonkey.com/r/MLMYBG7">this application</a> by Friday, June 30, 2023. We will be in touch by July 14th.</b> </p>
+                <p className='indent-more p'>
+                If you have any questions, please reach out to Jennie, the WLC’s incredible Virtual Assistant, at assistant@wlcsocialimpact.com.
+                </p>
             </div>
 
-            {/* <a href="https://forms.gle/3mLLHw4STJnPpJWN6"><div className="button">APPLY</div></a> */}
+            <a href="https://www.surveymonkey.com/r/MLMYBG7"><div className="button">APPLY</div></a>
             
             
 
